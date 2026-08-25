9b75e456a6f3aa8045a8ba85c785ba659b726649
jtcvmarie <jtc.vmarie@gmail.com>
2026-08-24 18:39:52 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </head>
       <body>{children}</body>
     </html>
   );
